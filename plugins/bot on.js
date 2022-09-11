fbe56ca21a580ff2e0c2ff58b691fb38eaee208f
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-09-12 07:27:24 +0800

    Update bot on.js

--- plugins/bot on.js
+++ plugins/bot on.js
@@ -1,9 +1,10 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Uaaaaawh🥱, Maap kak Kanna tadi ketiduran😊* ')
-}
+    m.reply(' *Uaaaaawh🥱, Maap kak OHLX tadi ketiduran😊* ')
+}
+
 handler.tags = ['main']
-handler.command = /^(kannaon)$/i
+handler.command = /^(ohlxon)$/i
 
 handler.admin = true
 
